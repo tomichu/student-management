e474000ffc0e8fbc3e4c2c1f335d30313cdc9386
ST <shugentomita@gmail.com>
2016-10-13 18:09:03 +0000

    Revert the development file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,12 +35,10 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-=begin	
 	config.baukis = {
 		staff: { host: 'baukis.example.com', path: '' },
 		admin: { host: 'baukis.example.com', path: 'admin' },
 		customer: { host: 'example.com', path: 'mypage' },
 		restrict_ip_addresses: false
 	}
-=end
 end
